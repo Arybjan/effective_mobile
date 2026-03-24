13aed5a71dd43721271fb137cbe83205abd02769
Арыбжан Азизов <arybjen92@gmail.com>
2026-03-24 19:16:55 +0600

    done registerview

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from django.db import transaction
+from .serializers import UserProfileSerializer, RegisterSerializer
+from apps.access.models import Role
+from .models import User, UserSession
 
-# Create your views here.
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    @transaction.atomic
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        default_role = Role.objects.get(name="user")
+
+        user = User.objects.create(
+            first_name=serializer.validated_data(["first_name"]),
+            last_name=serializer.validated_data(["last_name"]),
+            middle_name=serializer.validated_data(["middle_name", ""]),
+            email=serializer.validated_data(["email"]),
+            password_hash=serializer.validated_data(["password"]),
+            role=default_role,
+        )
+
+        return Response(
+            {
+                "message": "Пользователь успешно зарегистрирован.",
+                "user": UserProfileSerializer(user).data,
+            },
+            status=status.HTTP_201_CREATED,
+        )
+    
