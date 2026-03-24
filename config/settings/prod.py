78a1554fd4807d0987b9102f84a4b6a7e6d20caa
Арыбжан Азизов <arybzhanazizov@MacBook-Air-Arybzan.local>
2026-03-24 13:28:43 +0600

    first commit

---
+++ config/settings/prod.py
@@ -0,0 +1,11 @@
+from .base import *
+import os
+
+DEBUG = False
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
