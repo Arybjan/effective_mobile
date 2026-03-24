81c0d498813c179ba2ea7b504efa0cf5ed340e9f
Арыбжан Азизов <arybzhanazizov@MacBook-Air-Arybzan.local>
2026-03-24 15:37:59 +0600

    func for generate and decode token

--- common/utils.py
+++ common/utils.py
@@ -14,3 +14,18 @@ def hash_password(raw_password: str) -> str:
 
 def check_password(raw_password: str, password_hash: str) -> bool:
     return bcrypt.checkpw(raw_password.encode("utf-8"), password_hash.encode("utf-8"))
+
+
+def generate_access_token(user_id: int, expires_minute: int = 60) -> str:
+    now = datetime.now(timezone.utc)
+    payload = {
+        "user_id": user_id,
+        "type": "access",
+        "iat": now,
+        "exp": now + timedelta(minutes=expires_minute),
+    }
+    return jwt.encode(payload, settings.SECRET_KEY, algorithm="SHA256")
+
+
+def decode_access_token(token: str) -> bool:
+    return jwt.decode(token, settings.SECRET_KEY, algorithms=["SHA256"])
