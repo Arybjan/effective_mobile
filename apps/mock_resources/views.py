0739c572023090bf9932c59717d4a17c19e08959
Арыбжан Азизов <arybjen92@gmail.com>
2026-03-26 01:15:52 +0600

    orders apiview

--- apps/mock_resources/views.py
+++ apps/mock_resources/views.py
@@ -56,7 +56,10 @@ class ProductView(APIView):
         user = request.user
 
         if not has_access(user, "products", "create"):
-            return Response({"detail": ""}, status=status.HTTP_403_FORBIDDEN)
+            return Response(
+                {"detail": "У вас нет доступа к созданию products"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
 
         new_item = {
             "id": len(MOCK_PRODUCTS) + 1,
@@ -64,5 +67,90 @@ class ProductView(APIView):
             "owner_id": user.id,
         }
         return Response(new_item, status=status.HTTP_201_CREATED)
-    
 
+
+class OrdersView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        role = get_role_rule(user, "orders")
+
+        if not role:
+            return Response(
+                {"detail": "Нет доступа к ресурсу orders"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        if role.read_all_permission:
+            return Response(MOCK_ORDERS, status=status.HTTP_200_OK)
+
+        if role.read_permission:
+            own_orders = [item for item in MOCK_ORDERS if item["owner_id"] == user.id]
+            return Response(own_orders, status=status.HTTP_200_OK)
+
+        return Response(
+            {"detail": "У вас нет доступа к просмотру orders"},
+            status=status.HTTP_403_FORBIDDEN,
+        )
+
+    def post(self, request):
+        user = request.user
+
+        if not has_access(user, "orders", "create"):
+            return Response(
+                {"detail": "У вас нет доступа к созданию orders"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        new_order = {
+            "id": len(MOCK_PRODUCTS) + 1,
+            "title": request.data.get("title", "New order"),
+            "owner_id": user.id,
+        }
+        return Response(new_order, status=status.HTTP_201_CREATED)
+
+    def putch(self, request):
+        user = request.user
+        order_id = request.data.get("id")
+
+        order = next((item for item in MOCK_ORDERS if item["id"] == order_id), None)
+        if not order:
+            return Response(
+                {"detail": "Order не найден"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        is_owner = order["owner"] == user.id
+
+        if not has_access(user, "orders", "update", is_owner=is_owner):
+            return Response(
+                {"detail": "У вас нет доступа к изменению orders"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        order["title"] = request.data.get("title", order["title"])
+        return Response(order, status=status.HTTP_200_OK)
+
+    def delete(self, request):
+        user = request.user
+        order_id = request.data.get("id")
+
+        order = next((item for item in MOCK_ORDERS if item["id"] == order_id), None)
+        if not order:
+            return Response(
+                {"detail": "Order не найден."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        is_owner = order["owner_id"] == user.id
+
+        if not has_access(user, "orders", "delete", is_owner=is_owner):
+            return Response(
+                {"detail": "У вас нет доступа к удалению orders"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        return Response(
+            {"message": f"Order {order_id} удален"},
+            status=status.HTTP_200_OK,
+        )
